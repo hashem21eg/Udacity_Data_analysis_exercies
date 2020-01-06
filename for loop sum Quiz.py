f9a4039008b8dc3e259d0e1097ddd24d9a800bf4
Hashem Omran <41966305+hashem21eg@users.noreply.github.com>
2020-01-07 00:44:43 +0300

    sum function for list
    using for loop to sum numbers in list

---
+++ for loop sum Quiz.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+def sum_list(input_list_to_sum):
+    sum = 0
+    for i in input_list_to_sum:
+        sum = sum + i
+    return sum
+
+
+# In[7]:
+
+
+sum_list([1,2,3,4,5,6,7,8,9,9])
+
+
+# In[ ]:
+
+
+
+
