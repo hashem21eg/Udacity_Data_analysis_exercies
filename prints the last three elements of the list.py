3fcb4d4d6c9412d9cc89d7030ceb3bbacb2fdfda
Hashem Omran <41966305+hashem21eg@users.noreply.github.com>
2020-01-06 22:03:14 +0300

    prints the last three elements of the list
    slicing list very simple syntax

---
+++ prints the last three elements of the list.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+eclipse_dates = ['June 21, 2001', 'December 4, 2002', 'November 23, 2003',
+                 'March 29, 2006', 'August 1, 2008', 'July 22, 2009',
+                 'July 11, 2010', 'November 13, 2012', 'March 20, 2015',
+                 'March 9, 2016']
+
+
+# In[7]:
+
+
+eclipse_dates[-4:-1]
+
+
+# In[8]:
+
+
+print(eclipse_dates[-4:-1])
+
+
+# In[ ]:
+
+
+
+
